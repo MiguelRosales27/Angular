b7f1fbc4bae8f51a01636e583c36323b1a93b7d5
TXT-35 <txt-35@TXT-35.local>
2021-06-25 09:57:49 -0700

    bagel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularLesson3';
+  title2 = 'Hobbies'
+  person = 'Miguel';
+  age = '16';
+  title3 = 'Favorite Movies'
+  textToChange = 'Movie';
+  hobbyArray = ["I like to play videogames", "I like to draw", "I like to binge watch series"];
 }
+
